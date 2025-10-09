fbedc4a951a028c7fcc04ffcd9531cb9bb17770c
LKBotond <l.kovacs.botond@gmail.com>
2025-10-09 13:46:06 +0300

    added: manifest.json

---
+++ scraper/Scraper.js
@@ -0,0 +1,12 @@
+//necesary Utility functions:
+async function sendMessage(message) {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(message, (response) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+}
