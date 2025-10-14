4a8af38ed8c1bb2080619f46c8248348b029d947
LKBotond <l.kovacs.botond@gmail.com>
2025-10-14 22:21:30 +0300

    organized code structure in messages.Js CredentialToken.js

--- storage/Messages.js
+++ storage/Messages.js
@@ -10,34 +10,3 @@ export function sendMessage(message) {
   });
 }
 
-export function sendTargetedMessage(message, target) {
-  return new Promise((resolve, reject) => {
-    chrome.tabs.sendMessage(target, message, (response) => {
-      if (chrome.runtime.lastError) {
-        reject(chrome.runtime.lastError);
-      } else {
-        resolve(response);
-      }
-    });
-  });
-}
-
-export function sendPortMessage(port, data) {
-  return new Promise((resolve, reject) => {
-    const handleMessage = (response) => {
-      port.onMessage.removeListener(handleMessage);
-      port.onDisconnect.removeListener(handleDisconnect);
-      resolve(response);
-    };
-
-    const handleDisconnect = () => {
-      port.onMessage.removeListener(handleMessage);
-      reject(new Error("Port disconnected before response"));
-    };
-
-    port.onMessage.addListener(handleMessage);
-    port.onDisconnect.addListener(handleDisconnect);
-
-    port.postMessage({ data });
-  });
-}
